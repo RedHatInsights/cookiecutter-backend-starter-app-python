7ab9c380d382e08791ede148c613537a9db8ed30
Matt Holder <mjholder96@gmail.com>
2023-08-17 07:51:54 -0400

    configure liveness readiness probes

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/views.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/views.py
@@ -1,13 +1,1 @@
-from django.http import JsonResponse
-import logging
 
-logger = logging.getLogger(__name__)
-
-
-def healthz(req):
-    health_data = {
-        "app_name": "{{cookiecutter.project_name}}",
-        "status": "running",
-    }
-
-    return JsonResponse(health_data)
