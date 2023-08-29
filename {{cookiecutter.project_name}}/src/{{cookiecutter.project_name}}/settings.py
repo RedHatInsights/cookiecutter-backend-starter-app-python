5dda9bb9efc66a0c0227d80d358d658b48b25d3c
Matt Holder <mjholder96@gmail.com>
2023-08-29 09:21:25 -0400

    remove unused import

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
-import sys
 from pathlib import Path
 from app_common_python import LoadedConfig, isClowderEnabled, loadConfig
 # flake8: noqa
