7dc250378796805007901b8ae2d5d9c1a88fb36c
Matt Holder <mjholder96@gmail.com>
2023-08-30 11:04:32 -0400

    one line

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/settings.py
@@ -35,8 +35,7 @@ if isClowderEnabled():
 
     # TODO: refactor into app-common lib
     try:
-        API_PATH = [ x.apiPath for x in cfg.endpoints if x.app == APP_NAME ].pop()
-        API_PATH = API_PATH.lstrip('/')
+        API_PATH = [ x.apiPath for x in cfg.endpoints if x.app == APP_NAME ].pop().lstrip('/')
     except IndexError:
         API_PATH = ""
 
